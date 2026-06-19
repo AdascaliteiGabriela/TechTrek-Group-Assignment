2f4616f6bc8fa9afaffa101e5764b82a2ab8b79d
AdascaliteiGabriela <gabriela.adascalitei1@gmail.com>
2026-06-19 14:15:31 +0300

    Initialized project

---
+++ splitter/src/test/java/com/task/splitter/SplitterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.splitter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
